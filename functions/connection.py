95e157a124619b599385928897bf983ef45cfb16
Justin van Ziel <justin.van.ziel@gmail.com>
2018-10-25 15:53:27 +0200

    Added an "server in progress" alert (yellow blinking light)

--- functions/connection.py
+++ functions/connection.py
@@ -6,7 +6,20 @@ led_Yellow = LED(4)
 
 def connected(status):
     while status:
-        sleep(1)
+        sleep(0.5)
         led_Yellow.on()
-        sleep(1)
+        sleep(0.5)
         led_Yellow.off()
+
+
+# Shows with a blinking light that the server is in progress
+def progress():
+
+    blink = 0
+
+    while blink < 5:
+        sleep(0.5)
+        led_Yellow.on()
+        sleep(0.5)
+        led_Yellow.off()
+        blink += 1
